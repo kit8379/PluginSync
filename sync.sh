986276f07ec8f6af4a84e575eb9f73f1548e7aa1
kit8379 <manhhkit@gmail.com>
2022-11-14 23:15:06 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAME="survival"
-PLUGIN=("MMOItems" "MythicMobs") 
-SERVERUUID=("0699421b-cc90-40d5-3253-f8b9ca7238ab" "329421b-cc90-40d5-3253-f8b9cb4738ab")
+PLUGIN=("" "" "")
+SERVERUUID=("" "" "")
 
 
 PLENGTH=${#PLUGIN[*]}
@@ -17,6 +17,7 @@ do
 		then
 			cp -r /var/lib/pterodactyl/volumes/${SERVERUUID[$j]}/plugins/${PLUGIN[$i]} /root/sync/${NAME}/${PLUGIN[$i]}
 		fi
-		unison /var/lib/pterodactyl/volumes/${SERVERUUID[$j]}/plugins/${PLUGIN[$i]} /root/sync/${NAME}/${PLUGIN[$i]} -auto -batch -repeat watch -prefer newer -fat >/dev/null 2>&1 &
+		unison /root/sync/${NAME}/${PLUGIN[$i]} /var/lib/pterodactyl/volumes/${SERVERUUID[$j]}/plugins/${PLUGIN[$i]} -auto -batch -repeat watch -prefer newer -fat >/dev/null 2>&1 &
 	done
 done
+
