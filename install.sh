4569646d28f3c9ae9c1bd2841008799641301d6f
kit8379 <manhhkit@gmail.com>
2022-11-16 23:52:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ sudo apt-get install unison -y
 sudo apt-get install cargo -y
 sudo cargo install unison-fsmonitor -y
 echo "Installation process is ended!"
+
+sudo crontab -l >/tmp/cron
+echo '@reboot /root/sync.sh' >>/tmp/cron
+sudo crontab /tmp/cron
+
