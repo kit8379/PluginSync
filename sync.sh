9714db8de05c136df4bea1fafed131ff1a518a08
kit8379 <manhhkit@gmail.com>
2022-11-16 23:36:45 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -13,6 +13,10 @@ do
 	for ((j=0; j<${SLENGTH}; j++))
 	do
 		echo "${SERVERUUID[$j]} ${PLUGIN[$i]}"
+		if [ ! -d "/root/sync/${NAME}" ] 
+		then
+			sudo mkdir ${NAME}
+		fi
 		if [ ! -d "/root/sync/${NAME}/${PLUGIN[$i]}" ] 
 		then
 			sudo cp -r /var/lib/pterodactyl/volumes/${SERVERUUID[$j]}/plugins/${PLUGIN[$i]} /root/sync/${NAME}/${PLUGIN[$i]}
